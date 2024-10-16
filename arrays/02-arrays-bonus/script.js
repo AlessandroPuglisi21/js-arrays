32d20d5ec9422a47e2e408f5c6d42dd717b5eedc
AlessandroPuglisi <alessandropuglisi2000@gmail.com>
2024-10-16 16:32:53 +0200

    ADDED: Bonus Exercise

--- arrays/02-arrays-bonus/script.js
+++ arrays/02-arrays-bonus/script.js
@@ -10,10 +10,24 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+const reversedTeachers = teachers
+reversedTeachers.reverse()
 
+console.log(reversedTeachers)
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = []
+longNames.forEach(nameLength => {
+  if(nameLength >= 5){
+    console.log(longNames)
+  }
+});
 
 // 3. Rimuovi 'Ed' dall'array teachers
+const index = teachers.indexOf('Ed')
+
+if (index !== -1) {
+  teachers.splice(index, 1)
+}
+
+console.log(teachers)
